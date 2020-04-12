2355cdf86fb59d1f14fec735bbe3846b0ff74812
Kaloyan Yosifov <kyosifov@2create.studio>
2020-04-12 19:41:58 +0300

    Add client

---
+++ client/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    'parser': '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/essential',
+    '@vue/standard',
+    '@vue/typescript/recommended',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    'space-before-function-paren': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'no-case-declarations': 'off',
+    'no-void': 'off',
+    'no-trailing-spaces': ['error', {
+      skipBlankLines: true,
+    }],
+    'comma-dangle': ['error', 'always-multiline', {
+      arrays: 'always',
+      objects: 'always',
+      imports: 'never',
+      exports: 'never',
+      functions: 'never',
+    }],
+    indent: ['error', 4, { 'SwitchCase': 1 }],
+    semi: ['error', 'always'],
+  },
+};
