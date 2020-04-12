95019a64ff23ee32d7653cba185ccd8289041387
Kaloyan Yosifov <kyosifov@2create.studio>
2020-04-12 11:24:25 +0300

    Set default value for db if it doesnt exist

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,11 +1,25 @@
 /**
  * External dependencies.
  */
+import Todo from '@/features/todos/models/Todo';
+
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const path = require('path');
+const fs = require('fs');
+
+const dbFile = path.resolve(__dirname, 'db.json');
+const dbExists = fs.existsSync(dbFile);
+
+if (!dbExists) {
+    fs.createWriteStream(dbFile).write('');
+}
 
 const adapter = new FileSync(path.resolve(__dirname, 'db.json'));
 const db = low(adapter);
+
+if (!dbExists) {
+    db.defaults({ [Todo.getTable()]: [] }).write();
+}
 
 export default db;
